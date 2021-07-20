7dc7e53b55adeb437d9f77dcd92838d684d053e3
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-20 09:30:34 -0300

    Class 240 - Adding State Slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
 import { createStore } from 'redux';
 
 const initialState = { counter: 0, showCounter: true };
 
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter += 1;
+    },
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+    decrement(state) {
+      state.counter -= 1;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export const INCREMENT = 'increment';
+export const INCREASE = 'increase';
+export const DECREMENT = 'decrement';
+export const TOGGLE = 'toggle';
+
 const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment')
+  if (action.type === INCREMENT)
     return { ...state, counter: state.counter + 1 };
-  if (action.type === 'increase')
+  if (action.type === INCREASE)
     return { ...state, counter: state.counter + action.amount };
-  if (action.type === 'decrement')
+  if (action.type === DECREMENT)
     return { ...state, counter: state.counter - 1 };
-  if (action.type === 'toggle') {
+  if (action.type === TOGGLE) {
     return { ...state, showCounter: !state.showCounter };
   }
   return state;
